dafb8060f905d9289adb4c48343f0506b1494994
Hamza <zaki@MacBook-Pro.local>
2024-03-23 15:33:02 +0000

    final

--- test.c
+++ test.c
@@ -14,36 +14,77 @@
 #include <unistd.h>
 
 
-int main() {
-    int fd1, fd2;
+#include <stdio.h>
+#include <unistd.h>
 
-    // Open a file
-    fd1 = open("example.txt", O_CREAT | O_RDWR);
-    if (fd1 == -1) {
-        perror("open");
-        return 1;
-    }
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-    // Duplicate the file descriptor to file descriptor number 10
-    fd2 = dup2(fd1, 10);
-    if (fd2 == -1) {
-        perror("dup2");
-        close(fd1);
-        return 1;
-    }
-
-    printf("File descriptors: %d, %d\n", fd1, fd2);
-
-    // Close the original file descriptor
-    close(fd1);
-
-    // Use the duplicated file descriptor (fd2)...
-
-    // Close the duplicated file descriptor
-    close(fd2);
-
-    return 0;
+//	Utility function for write
+void	writestr(int fd, const char *str)
+{
+	write(fd, str, strlen(str));
 }
 
+//	Main
+int	main(void)
+{
+	int	pipefd[2];  // Stores the pipe's fds:
+			    //	- pipefd[0]: read only
+			    //	- pipefd[1]: write only
+	pid_t	pid;	// Stores fork's return value
+	char	buf;	// Stores characters read by read
+
+//	Create a pipe. Stop eveything on failure.
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+//	Create a child process
+	pid = fork();
+	if (pid == -1) // Failute, stop everything
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0) // Child process
+	{
+	//	Close the unused write end
+		close(pipefd[1]);
+		writestr(STDOUT_FILENO, "Child: What is the secret in this pipe?\n");
+		writestr(STDOUT_FILENO, "Child: \"");
+	//	Read characters from the pipe one by one
+		while (read(pipefd[0], &buf, 1) > 0)
+		{
+			// Write the read character to standard output
+			write(STDOUT_FILENO, &buf, 1);
+		}
+		writestr(STDOUT_FILENO, "\"\n");
+		writestr(STDOUT_FILENO, "Child: Wow! I must go see my father.\n");
+	//	Close the read end of the pipe
+		close(pipefd[0]);
+		exit(EXIT_SUCCESS);
+	}
+	else	// Parent process
+	{
+	//	Close unused read end
+		close(pipefd[0]);
+		writestr(STDOUT_FILENO, "Parent: I'm writing a secret in this pipe...\n");
+	//	Write into the pipe
+		writestr(pipefd[1], "\e[33mI am your father mwahahaha!\e[0m");
+	//	Close write end of the pipe (reader will see EOF)
+		close(pipefd[1]);
+	//	Wait for child
+		wait(NULL);
+		writestr(STDOUT_FILENO, "Parent: Hello child!\n");
+		exit(EXIT_SUCCESS);
+	}
+}
 
 
